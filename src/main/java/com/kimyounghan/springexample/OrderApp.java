9b1f3f1d2f98e0b45d9f4d2ef8c5df1eb548f49a
joranegs92 <joranegs92@gmail.com>
2023-09-07 00:53:09 +0900

    5.주문과 할인 도메인 개발 테스트

---
+++ src/main/java/com/kimyounghan/springexample/OrderApp.java
@@ -0,0 +1,25 @@
+package com.kimyounghan.springexample;
+
+import com.kimyounghan.springexample.member.Grade;
+import com.kimyounghan.springexample.member.Member;
+import com.kimyounghan.springexample.member.MemberService;
+import com.kimyounghan.springexample.member.MemberServiceImpl;
+import com.kimyounghan.springexample.order.Order;
+import com.kimyounghan.springexample.order.OrderService;
+import com.kimyounghan.springexample.order.OrderServiceImpl;
+
+public class OrderApp {
+	public static void main(String[] args) {
+		MemberService memberService = new MemberServiceImpl();
+		OrderService orderService = new OrderServiceImpl();
+
+		Long memberId =1L;
+		Member member = new Member(memberId, "memberA", Grade.VIP);
+		memberService.join(member);
+
+		Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+		System.out.println(order);
+		System.out.println(order.caclutaerPrice());
+	}
+}
